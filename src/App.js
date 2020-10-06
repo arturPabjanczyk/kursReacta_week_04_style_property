e86d40837eb9b9009c6b7603451ca7ef0784967e
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-06 19:40:21 +0200

    Modify style object

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
     const buttonStyle = {
         "font-size": "30px"
     }
+    // buttonStyle["font-size"] = "20px";
+    // buttonStyle.fontSize = "20px";
+    buttonStyle.fontSize = 20;
     return (
         <div className="App">
             <header className="App-header">
